6a63c817089b105eb7015d45fc1cc78978ed6f87
ranwawa <ranwawa@users.noreply.github.com>
2023-05-14 12:20:53 +0000

    deploy: 0044dda7639930a1a80c57d29b0953626494e59d

--- assets/rules_format_prose-wrap.md.d3893b2e.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as r,o as t}from"./app.81b55141.js";const f=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"rules/format/prose-wrap.md","lastUpdated":1680654961000}'),a={name:"rules/format/prose-wrap.md"};function o(s,p,c,n,d,_){return t(),r("div")}const i=e(a,[["render",o]]);export{f as __pageData,i as default};
